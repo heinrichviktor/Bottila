ad23c73718df960d32890f131242693568ab0775
heinrichviktor <42236070+heinrichviktor@users.noreply.github.com>
2018-10-14 18:11:50 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,60 @@
-const Discord = require('discord.js');
+const key = require("./secretkey.js");
+const Discord = require("discord.js");
+const snoowrap = require("snoowrap");
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const bot = new Discord.Client();
-
-var key = require("./secretkey.js");
-
-//console.log(key);
-bot.on('message', (message) => {
-
-    if(message.content == 'b!szia'){
-        //message.channel.sendMessage('SZIAAAAAA');
-        message.reply('SZIAAAAA');
-    }
-
+const r = new snoowrap({
+  userAgent: "put your user-agent string here",
+  clientId: key.redditKey,
+  clientSecret: key.redditClientSecret,
+  refreshToken: key.redditRefreshToken
 });
-bot.login(key.key);
+
+bot.login(key.discordKey);
+
+var xmlhttp = new XMLHttpRequest();
+var subreddit = "dankmemes";
+//.slice(6);
+//r.getRandomSubmission('dankmemes').then(console.log);
+bot.on("message", message => {
+  //console.log(message.content);
+  //if(message.content == 'b!img'){
+  //if(message.content.includes("b!img")){
+  if (message.content == "b!img") {
+    subreddit = "dankmemes";
+    sendImage();
+  } else if (message.content.startsWith("b!img")) {
+    subreddit = message.content.slice(6);
+    sendImage();
+  } else {
+  }
+
+  function sendImage() {
+    xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        var myObj = JSON.parse(this.responseText);
+        var img = myObj.data.children[0].data.url;
+        var authorText = myObj.data.children[0].data.title;
+        message.channel.send(authorText);
+
+        if (myObj.data.children[0].data.content_categories == "writing") {
+          console.log("szöveg");
+          message.channel.send("Olvasd el tesa: " + myObj.data.children[0].data.url);
+        } else {
+          let picture = new Discord.RichEmbed().setImage(img);
+          message.channel.send(picture);
+          console.log("kép");
+        }
+        //console.log(myObj.data.children[0].data.selftext)
+        //
+      }
+    };
+    //console.log(subreddit);
+    xmlhttp.open(
+      "GET",
+      "https://www.reddit.com/r/" + subreddit + "/new.json?sort=new",
+      true
+    );
+    xmlhttp.send();
+  }
+});
---
+++ secretkey.js
@@ -0,0 +1,11 @@
+discordKey = "NTAwNzc3NDc0MzUxNjkzODMy.DqPxKg.YUNZUB512DxtWfmUsTDvnkKc_60";
+redditKey = "65Up7DD02jAKiw";
+redditClientSecret = "L7tFPXtS4EhwwH5nuNDsrIXwCAI";
+redditRefreshToken = "188528078351-UbCgmKbgChP3op6XNfuLE6RS0m4";
+
+module.exports = {
+  discordKey,
+  redditKey,
+  redditClientSecret,
+  redditRefreshToken
+};
