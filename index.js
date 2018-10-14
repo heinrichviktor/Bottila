c185905cbc120f5867a64c1b2f7f2dfe8c083a43
harsanyizoli <h.zoli098@gmail.com>
2018-10-14 21:18:22 +0200

    subreddit

--- index.js
+++ index.js
@@ -14,49 +14,37 @@ const r = new snoowrap({
 
 bot.login(key.discordKey);
 
-var xmlhttp = new XMLHttpRequest();
-var subreddit = "dankmemes";
-//.slice(6);
-//r.getRandomSubmission('dankmemes').then(console.log);
 bot.on("message", message => {
   //console.log(message.content);
   //if(message.content == 'b!img'){
   //if(message.content.includes("b!img")){
-  if (message.content == "b!img") {
-    subreddit = "dankmemes";
-    sendImage(subreddit);
-  } else if (message.content.startsWith("b!img")) {
-    subreddit = message.content.slice(6);
-    sendImage(subreddit);
-  } else {
+  msg = message.content.split(" ");
+  if( msg[0] == "b!img" ){
+    /*
+    url = "https://i.imgur.com/CQdnUuj.jpg"
+    sendImg(url, message);
+    */
+    sub = msg[1];
+    getImages(sub);
   }
 
-  function sendImage(subreddit) {
-    xmlhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        var myObj = JSON.parse(this.responseText);
-        var img = myObj.data.children[0].data.url;
-        var authorText = myObj.data.children[0].data.title;
-        message.channel.send(authorText);
-
-        if (myObj.data.children[0].data.content_categories == "writing") {
-          console.log("szöveg");
-          message.channel.send("Olvasd el tesa: " + myObj.data.children[0].data.url);
-        } else {
-          let picture = new Discord.RichEmbed().setImage(img);
-          message.channel.send(picture);
-          console.log("kép");
-        }
-        //console.log(myObj.data.children[0].data.selftext)
-        //
+  function getImages(sub){
+    r.getHot(sub, {limit: 100}).map(post => post.url)
+      .filter(url => url.match(/(https:|http:)[a-z0-9/\.]+(png|jpg)/gi))
+      //.then(urls => console.log(urls[getRandomInt(urls.length)]))
+      .then(urls => sendImg(urls[getRandomInt(urls.length)]));
+  
+      let getRandomInt = function(max) {
+        return Math.floor(Math.random() * Math.floor(max));
       }
-    };
-    //console.log(subreddit);
-    xmlhttp.open(
-      "GET",
-      "https://www.reddit.com/r/" + subreddit + "/new.json?sort=new",
-      true
-    );
-    xmlhttp.send();
   }
+
+  function sendImg(url){
+    let picture = new Discord.RichEmbed().setImage(url);
+    message.channel.send(picture);
+  }
+  //TODO: nincs url
 });
+
+
+
